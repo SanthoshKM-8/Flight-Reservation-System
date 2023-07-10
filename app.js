ffbf7e78db2170b888e9ed00562a7fe643ecdb51
Santhosh K M <100404703+SanthoshKM-8@users.noreply.github.com>
2023-07-10 10:38:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.post("/login", function(req, res) {
         }
     });
     console.log(req.body.email);
-    console.log(req.body.password);
+    // console.log(req.body.password);
 });
 
 app.get("/signup", function(req, res) {
