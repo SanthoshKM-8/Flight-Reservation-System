a6ce487fb14953925c31c120c4e15f267a05204a
Santhosh K M <100404703+SanthoshKM-8@users.noreply.github.com>
2023-07-15 13:36:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.static("public"));
 
 let email, name;
 const pool = mysql.createPool({
-  host: 'localhost',
+  host: '127.0.0.1',
   port: '3306',
   user: 'root',
   password: 'SanthoshDB@2',
